11baf8a09890936e776c8413af73abd26fd5611e
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-08-06 10:16:27 -0400

    begins building out show action for show

--- app/controllers/api/v1/representatives_controller.rb
+++ app/controllers/api/v1/representatives_controller.rb
@@ -1,5 +1,8 @@
 class RepresentativesController < ApplicationController
   def show
-    
+   url = "https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=#{zipcode}&key=API_KEY"
+   response = HTTParty.get(url)
+   render json: response
+   # I left off by commenting out the route and trying to set up the controller action to successfully call the api with the zip code parameter provided by the front end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  post "/api/v1/find_reps", to: "api/v1/representatives#show"
+  # post "/api/v1/find_reps", to: "api/v1/representatives#show"
 
   get '/api/v1/representatives/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
