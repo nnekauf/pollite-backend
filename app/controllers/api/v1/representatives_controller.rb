cd3841d31228082be3c8e4af2ba26a10437bf2eb
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-08-06 11:05:54 -0400

    successfully retrieves response from api and renders it in index

--- app/controllers/api/v1/representatives_controller.rb
+++ app/controllers/api/v1/representatives_controller.rb
@@ -1,8 +1,15 @@
 class Api::V1::RepresentativesController < ApplicationController
-  def show
-   url = "https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=#{zipcode}&key=API_KEY"
-   response = HTTParty.get(url)
-   render json: response
-   # I left off by commenting out the route and trying to set up the controller action to successfully call the api with the zip code parameter provided by the front end
+  
+  def index
+    url = "https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=07307&key=#{ENV["API_KEY"]}"
+    response = HTTParty.get(url)
+    render json: response   
   end
+
+  # def show
+  # url = "https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=#{zipcode}&key=API_KEY"
+  # response = HTTParty.get(url)
+  # render json: response 
+  #  # I left off by commenting out the route and trying to set up the controller action to successfully call the api with the zip code parameter provided by the front end
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # post "/api/v1/find_reps", to: "api/v1/representatives#show"
 
-  get '/api/v1/representatives/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get '/api/v1/representatives/show'
+  namespace :api do
+    namespace :v1 do
+      resources :representatives, only: [:index] 
+    end
+  end
 end
