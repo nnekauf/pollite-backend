696476b187ebf1c7b4feb21c956432a5976da8c1
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-08-06 11:23:15 -0400

    comfigures show route to dynamically allow zipcode

--- app/controllers/api/v1/representatives_controller.rb
+++ app/controllers/api/v1/representatives_controller.rb
@@ -6,10 +6,11 @@ class Api::V1::RepresentativesController < ApplicationController
     render json: response   
   end
 
-  # def show
-  # url = "https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=#{zipcode}&key=API_KEY"
-  # response = HTTParty.get(url)
-  # render json: response 
+  def show
+  
+  url = "https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=#{params[:id]}&key=#{ENV["API_KEY"]}"
+  response = HTTParty.get(url)
+  render json: response 
   #  # I left off by commenting out the route and trying to set up the controller action to successfully call the api with the zip code parameter provided by the front end
-  # end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # get '/api/v1/representatives/show'
   namespace :api do
     namespace :v1 do
-      resources :representatives, only: [:index] 
+      resources :representatives, only: [:index, :show] 
     end
   end
 end
