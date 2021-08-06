643044e236224c7805d05aa636f3e80cf352fc63
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-08-06 14:04:52 -0400

    renders city, state, zipcode, and officals from api response

--- app/controllers/api/v1/representatives_controller.rb
+++ app/controllers/api/v1/representatives_controller.rb
@@ -14,12 +14,17 @@ class Api::V1::RepresentativesController < ApplicationController
   city = response["normalizedInput"]["city"]
   state = response["normalizedInput"]["state"]
   zip = response["normalizedInput"]["zip"]
-  name = response["offices"][0]["name"]
-  index = response["offices"][0]["officialIndices"]
-  # office = {name: response["offices"]["name"], index: response["offices"]["officialIndices"]}
-  
+  # name = response["offices"][0]["name"]
+  # index = response["offices"][0]["officialIndices"]
+  # find the official whose index number equals the officvialindices value within the offices array
+  # index = response["offices"].map {|n| n["officialIndices"]}
+  # name = response["offices"].map {|n| n["name"]}
+  # person = response["offices"].map {|p| p.map {|k, v| k v if k == "name" || k == "officialIndices"}}
+  # offices = {name:name, index: index}
   officials = response["officials"]
-  render json: {city: city, state: state, zipcode: zip, offices: {name:name, index: index}, officials: officials}
+  # binding.pry
+  # person = 
+  render json: {city: city, state: state, zipcode: zip, officials: officials}
   # binding.pry
 
   # response = HTTParty.get("https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=#{params[:id]}&key=#{ENV["API_KEY"]}",
